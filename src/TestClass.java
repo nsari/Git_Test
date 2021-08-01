305e5c1576ea6d03808a2ec4e920b7cb6b93a2fe
nsari <nuraysari.pharm@gmail.com>
2021-07-31 23:47:05 -0500

    i made these thibgs

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,7 +2,7 @@ public class TestClass {
     public static void main(String[] args) {
     System.out.println("Hello git hub");
         System.out.println("Hello from git hub");
-
+        System.out.println("Hello frfgfgfgdfgdgom git hub");
 /*
 echo "# Git_Test" >> README.md
 git init
@@ -10,6 +10,7 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/nsari/Git_Test.git
+git branch -M main
 git push -u origin main
  */
 
