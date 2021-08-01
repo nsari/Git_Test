def351610cf25a96db93f93d25058ad7f98a7ceb
nsari <nuraysari.pharm@gmail.com>
2021-07-31 22:57:25 -0500

    first version

---
+++ src/TestClass.java
@@ -0,0 +1,16 @@
+public class TestClass {
+    public static void main(String[] args) {
+    system.out.println("Hello git hub");
+
+/*
+echo "# Git_Test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nsari/Git_Test.git
+git push -u origin main
+ */
+
+    }
+}
